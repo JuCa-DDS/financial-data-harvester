180552a0cd2eff5dd90aab6aed1377ef06804a49
Juan Carlos <juancar.hernanrangel@gmail.com>
2025-07-29 21:13:37 -0600

    Iniciando FinancialData_Harvester

---
+++ dashboard/app.py
@@ -0,0 +1,99 @@
+import streamlit as st
+import datetime
+import pandas as pd
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from data_scraper.scraper import Download_YahooFinance
+
+class FinancialDashboardApp:
+    def __init__(self):
+        self.df = pd.DataFrame({'Col1':[1, 2, 3], 'Col2':[4, 5, 6]})
+        self.csv = self.convert_df(self.df)
+        self.scraper = Download_YahooFinance()
+        self.current_report = None
+        self.current_price = None
+
+    @st.cache_data
+    def convert_df(_self, df):
+        return df.to_csv().encode('utf-8')
+    
+    def __generate_report(self, ticker):
+        with st.spinner('Downloading info, please wait...'):
+            try:
+                info = self.scraper.get_info(ticker)
+                return info
+            except Exception as e:
+                st.error(f'Error con {ticker}: {str(e)}')
+    
+    def __generate_price(self, ticker, start_date, end_date):
+        price = self.scraper.get_price(ticker, start_date, end_date)
+        return price
+    
+    def render(self):
+        st.title('Financial Data Harvester')
+
+        col1, col2 = st.columns(2)
+        col3, col4 = st.columns(2)
+        col5, col6 = st.columns([3, 1])
+
+        with col1:
+            company = st.multiselect(
+                'Select a company', 
+                ['TSLA', 'AMZN', 'AAPL', 'JPM'], 
+                default=['TSLA']
+            )
+
+        with col2:
+            date_range = st.date_input(
+                'Select Date Range', 
+                (
+                    datetime.date(2023, 1, 1),
+                    datetime.date(2024, 1, 1)
+                ),
+                    datetime.date(2020, 1, 1),
+                    datetime.datetime.today()
+            )
+
+        with col3:    
+            mostrar_rsi = st.checkbox('Show RSI', value=False)
+            mostrar_sma = st.checkbox('Show SMA 50/200', value=False)
+            mostrar_volumne = st.checkbox('Volumen', value=False)
+
+        with col4:
+            button_report = st.button('Generate Report')
+            if button_report:
+                self.current_report = self.__generate_report(company[0])
+                print(self.current_report)
+                self.current_price = self.__generate_price(company[0], date_range[0], date_range[1])
+
+            if self.current_report is not None:
+                st.download_button(
+                        label='Download PDF Report',
+                        data=self.csv,
+                        file_name='data.csv',
+                        mime='text/csv'
+                )
+        
+        with col5:
+            if self.current_price is not None:
+                st.line_chart(
+                    data=self.current_price,
+                    x='Date',
+                    y='Close'
+                )
+        
+        with col6:
+            if self.current_price is not None:
+                st.metric(label='Trailing P/E', value=self.current_report['Trailing P/E'])
+                st.metric(label='Beta', value=self.current_report['Beta'])
+                st.metric(label='52 Week Change', value=self.current_report['52 Week Change'])
+        
+
+def main():
+    app = FinancialDashboardApp()
+    app.render()
+
+if __name__ == '__main__':
+    main()
---
+++ data_scraper/scraper.py
@@ -0,0 +1,99 @@
+import requests
+from requests.exceptions import RequestException
+import os
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import time
+from dotenv import load_dotenv
+from lxml import html
+
+load_dotenv()
+API = os.getenv('SCRAPERAPI_KEY')
+
+class Download_YahooFinance:
+    def __init__(self):
+        self.URL_QUOTE = 'https://finance.yahoo.com/quote'
+        self.URL_API = 'https://api.scraperapi.com/'
+        self._PAYLOAD = {
+            'api_key':API,
+            'premium':'true'
+        }
+
+    @property
+    def PAYLOAD(self):
+        return self._PAYLOAD.copy()
+    
+    def __get_quote(self, ticker):
+        url_ticker = os.path.join(self.URL_QUOTE, f'{ticker}')
+        url_statistics = os.path.join(url_ticker, 'key-statistics')
+        payload_ = self.PAYLOAD
+        payload_['url'] = url_statistics
+
+        for attempt in range(3):
+            try:
+                response = requests.get(self.URL_API, params=payload_, timeout=30)
+                if response.status_code == 200:
+                    return html.fromstring(response.content)
+                else:
+                    print('Dentro If')
+            except RequestException as e:
+                print(f'Intento {attempt+1} fallido: {e}')
+            time.sleep(1 + attempt)
+        return None
+    
+    def __extract_metrics(self, tree):
+        # market_cap = tree.xpath('//*[@id="nimbus-app"]/section/section/section/article/section[2]/div/table/tbody/tr[1]/td[2]/text()')
+        market_cap = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/section[2]/div/table/tbody/tr[1]/td[2]/text()')
+        enterprise_value = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/section[2]/div/table/tbody/tr[2]/td[2]/text()')
+        trailing_pe = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/section[2]/div/table/tbody/tr[3]/td[2]/text()')
+        forward_pe = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/section[2]/div/table/tbody/tr[4]/td[2]/text()')
+        peg_ratio_5yr = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/section[2]/div/table/tbody/tr[5]/td[2]/text()')
+        price_sales = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/section[2]/div/table/tbody/tr[6]/td[2]/text()')
+        price_book = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/section[2]/div/table/tbody/tr[7]/td[2]/text()')
+        enterprise_value_revenue = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/section[2]/div/table/tbody/tr[8]/td[2]/text()')
+        enterprise_value_ebitda = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/section[2]/div/table/tbody/tr[9]/td[2]/text()')
+
+        profit_margin = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/article/div/section[1]/div/section[2]/table/tbody/tr[1]/td[2]/text()')
+        operating_margin = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/article/div/section[1]/div/section[2]/table/tbody/tr[2]/td[2]/text()')
+
+        revenue = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/article/div/section[1]/div/section[4]/table/tbody/tr[1]/td[2]/text()')
+        revenue_per_share = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/article/div/section[1]/div/section[4]/table/tbody/tr[2]/td[2]/text()')
+        quarterly_revenue_growth = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/article/div/section[1]/div/section[4]/table/tbody/tr[3]/td[2]/text()')
+        gross_profit = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/article/div/section[1]/div/section[4]/table/tbody/tr[4]/td[2]/text()')
+        ebitda = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/article/div/section[1]/div/section[4]/table/tbody/tr[5]/td[2]/text()')
+
+        beta = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/article/div/section[2]/div/section[1]/table/tbody/tr[1]/td[2]/text()')
+        week_change_52 = tree.xpath('//*[@id="nimbus-app"]/section/section/section/section/article/div/section[2]/div/section[1]/table/tbody/tr[2]/td[2]/text()')
+
+        return {
+            'Market_Cap': market_cap[0] if len(market_cap) == 1 else None,
+            'Enterprise Value': enterprise_value[0] if len(enterprise_value) == 1 else None,
+            'Trailing P/E': trailing_pe[0] if len(trailing_pe) == 1 else None,
+            'Forward P/E': forward_pe[0] if len(forward_pe) == 1 else None,
+            'PEG Ratio 5yr Expected': peg_ratio_5yr[0] if len(peg_ratio_5yr) == 1 else None,
+            'Price/Sales': price_sales[0] if len(price_sales) == 1 else None,
+            'Price/Book': price_book[0] if len(price_book) == 1 else None,
+            'Enterprise Value/Revenue': enterprise_value_revenue[0] if len(enterprise_value_revenue) == 1 else None,
+            'Enterprise Value/EBITDA': enterprise_value_ebitda[0] if len(enterprise_value_ebitda) == 1 else None,
+            'Profit Margin': profit_margin[0] if len(profit_margin) == 1 else None,
+            'Operating Margin': operating_margin[0] if len(operating_margin) == 1 else None,
+            'Revenue': revenue[0] if len(revenue) == 1 else None,
+            'Revenue Per Share': revenue_per_share[0] if len(revenue_per_share) == 1 else None,
+            'Quarterly Revenue Growth': quarterly_revenue_growth[0] if len(quarterly_revenue_growth) == 1 else None,
+            'Gross Profit': gross_profit[0] if len(gross_profit) == 1 else None,
+            'EBITDA': ebitda[0] if len(ebitda) == 1 else None,
+            'Beta': beta[0] if len(beta) == 1 else None,
+            '52 Week Change': week_change_52[0] if len(week_change_52) == 1 else None
+        }
+    
+    def get_info(self, ticker):
+        tree = self.__get_quote(ticker)
+        info = self.__extract_metrics(tree)
+        return info
+    
+    def get_price(self, ticker, start_date, end_date):
+        close_price = yf.download(ticker, start_date, end_date)
+        close_price = close_price.reset_index()
+        close_price.columns = ['Date', 'Close', 'High', 'Low', 'Open', 'Volume']
+        return close_price
