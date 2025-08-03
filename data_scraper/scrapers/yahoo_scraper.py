9eaefdeec27b792c334f07ba605ad5bec11924c5
Juan Carlos <juancar.hernanrangel@gmail.com>
2025-08-02 21:41:19 -0600

    Refactorizar Yahoo Parser

---
+++ data_scraper/scrapers/yahoo_scraper.py
@@ -0,0 +1,93 @@
+import requests
+import time
+from requests.exceptions import RequestException
+from urllib.parse import urljoin
+from lxml import html
+
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+
+class YahooFinanceScraper:
+    def __init__(self, api):
+        self.base_url = 'https://finance.yahoo.com/quote/'
+        self.api_url = 'https://api.scraperapi.com/'
+        self.api_key = api
+
+        self._payload = {
+            'api_key':self.api_key,
+            'premium':'true'
+        }
+
+    @property
+    def payload(self):
+        return self._payload.copy()
+    
+    def get_statistics_page(self, ticker):
+        url_ticker = urljoin(self.base_url, f'{ticker}/')
+        url_statistics = urljoin(url_ticker, 'key-statistics')
+        payload_ = self.payload
+        payload_['url'] = url_statistics
+
+        for attempt in range(3):
+            try:
+                response = requests.get(self.api_url, params=payload_, timeout=30)
+                if response.status_code == 200:
+                    return html.fromstring(response.content)
+                else:
+                    print('Logger')
+            except RequestException as e:
+                print(f'Intento {attempt+1} fallido: {e}')
+            time.sleep(1 + attempt)
+        return None
+    
+    def get_news_feed(self, ticker):
+        url_news = f'http://feeds.finance.yahoo.com/rss/2.0/headline?s={ticker}&region=US&lang=en-US'
+        headers = {'User-Agent':'Mozilla/5.0'}
+        
+        try:
+            response = requests.get('GET', url_news, headers=headers, data={}).text
+            root = ET.fromstring(response.content)
+
+            items = []
+            for item in root.findall('.//item'):
+                title = item.find('title').text
+                link = item.find('link').text
+                pub_date = item.find('pubDate').text
+                items.append({'title':title, 'link':link, 'published':pub_date})
+            return items
+        except Exception as e:
+            return []
+        
+    def get_article_text(self, url):
+        payload_ = self.payload
+        payload_['url'] = url
+
+        for attempt in range(3):
+            try:
+                response = requests.get(self.api_url, params=payload_, timeout=30)
+                if response.status_code == 200:
+                    soup = BeautifulSoup(response.text, 'html.parser')
+                    paragraphs = soup.find_all('p')
+                    full_text = '\n'.join(p.get_text() for p in paragraphs)
+                    return full_text
+                else:
+                    print('Respuesta Diferente')
+            except RequestException as e:
+                print(f'Intento {attempt+1} fallido: {e}')
+            time.sleep(1 + attempt)
+        return None
+    
+    def get_full_news(self, ticker):
+        feed_news = self.get_news_feed(ticker)
+        full_news = []
+
+        for item in feed_news:
+            link = item['link']
+            article_text = self.get_article_text(link)
+            full_news.append({
+                'ticker':ticker,
+                'link':link,
+                'article_text':article_text
+            })
+
+        return full_news
